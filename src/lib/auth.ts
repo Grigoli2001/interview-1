1994661fd2ef8b03167f4937bcbfeec1ef0b3a0a
grigoli2001 <gpatsatsiausa@gmail.com>
2026-02-19 18:15:50 +0100

    Set up the db and installed necessary packages

---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+// import { AuthOptions } from "next-auth";
+// import Credentials from "next-auth/providers/credentials";
+
+// export const authOptions:AuthOptions = {
+//     providers: [
+//         Credentials({
+
+//         })
+//     ]
+// }
